5cf23778738d20db99075d150cc1db89cbb9fddd
Sanjithj <sanjithj591997@gmail.com>
2022-10-04 21:26:15 +0530

    Added: Create database

--- Mysql.java
+++ Mysql.java
@@ -4,6 +4,10 @@ public class Mysql {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Mysql Databse");
+		 create database Address_Book_Service;
+		 use Address_Book_Service;
+		 show databases;
+		 
 	}
 
 }
