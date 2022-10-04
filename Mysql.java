aa084e67a5388e06ee3ea9026f8916dcd0ae0981
Sanjithj <sanjithj591997@gmail.com>
2022-10-04 21:29:52 +0530

    Added: book table

--- Mysql.java
+++ Mysql.java
@@ -8,6 +8,17 @@ public class Mysql {
 		 use Address_Book_Service;
 		 show databases;
 		 
+		 create table address_book(
+				 
+		 first_name varchar(40),
+		 last_name varchar(30),
+		 address varchar(65),
+		 city varchar(30),
+		 state varchar(50),
+		 zip int,
+		 phone_number long,
+		 email varchar(20));
+				 
 	}
 
 }
